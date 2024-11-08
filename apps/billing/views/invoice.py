e046d7f0641cb6d4ade09f1e1419391c23974bab
Elkin <elkin@gmail.com>
2024-11-08 10:11:14 -0500

    Añadida la factura

--- apps/billing/views/invoice.py
+++ apps/billing/views/invoice.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from apps.billing.models import Invoice, InvoiceDetail
 from apps.products.models import Product
+from apps.shopping_cart.models import Cart, CartItem
 from django.contrib.auth import get_user_model
 import json
 
@@ -19,6 +20,7 @@ class InvoiceCreateView(View):
             tax = data.get('tax')
             total = data.get('total')
 
+            # Crear la factura
             invoice = Invoice.objects.create(
                 user=user,
                 subtotal=subtotal,
@@ -26,6 +28,7 @@ class InvoiceCreateView(View):
                 total=total
             )
 
+            # Agregar los detalles de los productos a la factura
             products_data = json.loads(data.get('products'))
             for product_id, product_info in products_data.items():
                 product = get_object_or_404(Product, pk=product_id)
@@ -40,6 +43,10 @@ class InvoiceCreateView(View):
                     price=price,
                     subtotal=detail_subtotal
                 )
+
+            # Vaciar el carrito del usuario
+            cart = Cart.objects.get(user=user)
+            CartItem.objects.filter(cart=cart).delete()
 
             return JsonResponse({'status': 'success'})
 
