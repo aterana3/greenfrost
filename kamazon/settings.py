48ababf4f051396486ffc30b27a12246639a7c41
Anthony Terán <aterana3@unemi.edu.ec>
2024-11-21 21:39:07 -0500

    eliminación de ajustes sin uso

--- kamazon/settings.py
+++ kamazon/settings.py
@@ -156,10 +156,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Temp files
-
-TEMP_ROOT = os.path.join(BASE_DIR, 'temp')
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
